3f1700f637ec459eab4a2b00ed3e4e571162c3ba
Navid Mitchell <navid@mitchellsoftware.net>
2023-03-03 10:17:24 -0700

    Updates to library

---
+++ project-library/src/main/groovy/{{basePackage|packageToPath}}/api/package-info.java
@@ -0,0 +1,7 @@
+
+@ContinuumPackage
+@Version("0.1.0")
+package {{basePackage}}.api;
+
+import org.kinotic.continuum.api.annotations.ContinuumPackage;
+import org.kinotic.continuum.api.annotations.Version;
